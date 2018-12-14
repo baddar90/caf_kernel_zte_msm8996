76de2db40d116e5fabd7837427243a1ce61f4e02
C457 <android.c357@gmail.com>
2018-12-13 21:24:15 -0600

    msm: mdss: Partially revert B12 Oreo changes
    * ZTE lcd common function is unused by all supported boards
    
    Change-Id: Ia009fb2215fa4255df8df1d29494d425da24274b

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -24,8 +24,6 @@
 #include <linux/string.h>
 #include <linux/display_state.h>
 
-#include "zte_lcd_common.h"
-
 #include "mdss_dsi.h"
 #include "mdss_debug.h"
 #ifdef TARGET_HW_MDSS_HDMI
@@ -3589,9 +3587,6 @@ int mdss_dsi_panel_init(struct device_node *node,
 		mutex_init(&zte_display_lock);
 		zte_display_init = 1;
 	}
-#ifdef CONFIG_ZTE_LCD_COMMON_FUNCTION
-	zte_lcd_common_func(ctrl_pdata, node);
-#endif
 
 	mdss_dsi_panel_lcd_proc(node);
 	return 0;
