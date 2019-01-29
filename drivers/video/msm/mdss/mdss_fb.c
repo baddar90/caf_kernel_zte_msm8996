6f79e4ac18ca6a19e14bc8d21ca3f9319b079872
C457 <android.c357@gmail.com>
2019-01-29 10:34:20 -0600

    msm: mdss: Cleanup panel
    * No one is debugging this code anymore
    * Remove unused proc entry
    
    Change-Id: I5f67b6757c50a2c93fbad5a1f613bd6105498a94

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -59,7 +59,6 @@
 #include "mdp3_ctrl.h"
 
 #include "mdss_livedisplay.h"
-u32 zte_frame_count;/*pan*/
 
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
@@ -1251,7 +1250,7 @@ static int mdss_fb_probe(struct platform_device *pdev)
 		pr_err("can't allocate framebuffer info data!\n");
 		return -ENOMEM;
 	}
-	zte_frame_count = 0;/*pan*/
+
 	mfd = (struct msm_fb_data_type *)fbi->par;
 	mfd->vr_mode = 0;		//zte jiangfeng add for vr mode
 	mfd->vr_mode_exiting = 0;		//zte jiangfeng add for vr mode
@@ -3711,7 +3710,6 @@ static int __mdss_fb_perform_commit(struct msm_fb_data_type *mfd)
 		else
 			mfd->pending_switch = false;
 	}
-	++zte_frame_count ;/*pan*/
 	if (fb_backup->disp_commit.flags & MDP_DISPLAY_COMMIT_OVERLAY) {
 		if (mfd->mdp.kickoff_fnc)
 			ret = mfd->mdp.kickoff_fnc(mfd,
