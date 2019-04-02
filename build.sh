88511b100167794f03da6c391897cd92dbddf4a5
baddar90 <mnbaddar1990@gmail.com>
2019-04-02 13:53:56 +0400

    remove obsolete folder and add build script

---
+++ build.sh
@@ -0,0 +1,36 @@
+export KBUILD_BUILD_USER=baddar90
+export KBUILD_BUILD_HOST=hidden
+export ARCH=arm64
+export CROSS_COMPILE=$HOME/Desktop/toolchain/bin/aarch64-linux-android-
+
+DIR=$(pwd)
+BUILD="$DIR/build"
+OUT="$DIR/out"
+#NPR=`expr $(nproc) + 1`
+
+echo "cleaning build..."
+if [ -d "$BUILD" ]; then
+rm -rf "$BUILD"
+fi
+if [ -d "$OUT" ]; then
+rm -rf "$OUT"
+fi
+
+echo "setting up build..."
+mkdir "$BUILD"
+make O="$BUILD" lineageos_axon7_defconfig
+
+echo "building kernel..."
+make O="$BUILD" -j30
+
+echo "building modules..."
+make O="$BUILD" INSTALL_MOD_PATH="." INSTALL_MOD_STRIP=1 modules_install
+rm $BUILD/lib/modules/*/build
+rm $BUILD/lib/modules/*/source
+
+mkdir -p $OUT/modules
+mv "$BUILD/arch/arm64/boot/Image.gz-dtb" "$OUT/Image.gz-dtb"
+find "$BUILD/lib/modules/" -name *.ko | xargs -n 1 -I '{}' mv {} "$OUT/modules"
+
+echo "Image.gz-dtb & modules can be found in $BUILD"
+
