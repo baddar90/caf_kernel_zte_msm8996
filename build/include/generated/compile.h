2ac8d275b9d5429a02a1c152e5f4f591f6d6df21
baddar90 <mnbaddar1990@gmail.com>
2019-04-03 11:07:38 +0400

    clean up the source

--- build/include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 0 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm64"
-#define UTS_VERSION "#0 SMP PREEMPT Wed Apr 3 10:14:50 +04 2019"
-#define LINUX_COMPILE_BY "baddar90"
-#define LINUX_COMPILE_HOST "hidden"
-#define LINUX_COMPILER "gcc version 8.2.0 (Buildroot 2018.11-rc2-00003-ga0787e9) "
