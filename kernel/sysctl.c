5bdbfaea2e25df6af061ce011d6e59d1f35e2049
baddar90 <mnbaddar1990@gmail.com>
2019-04-23 19:32:24 +0400

    Revert "sysctl: Nuke sched_boost feature"
    This reverts commit 9a4574b042d59c65c01eb2f762eff3ec001c0ff6.

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -528,7 +528,6 @@ static struct ctl_table kern_table[] = {
 	},
 #endif
 #endif
-#if 0
 	{
 		.procname	= "sched_boost",
 		.data		= &sysctl_sched_boost,
@@ -536,7 +535,6 @@ static struct ctl_table kern_table[] = {
 		.mode		= 0644,
 		.proc_handler	= sched_boost_handler,
 	},
-#endif
 #endif	/* CONFIG_SCHED_HMP */
 #ifdef CONFIG_SCHED_DEBUG
 	{
